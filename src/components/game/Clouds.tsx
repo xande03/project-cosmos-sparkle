df25b52fc82a1eb9ad3b25cccb4132f818509e13
xande03 <104875527+xande03@users.noreply.github.com>
2026-08-27 15:29:21 -0300

    feat: melhora visual e animação das nuvens no céu do jogo

--- src/components/game/Clouds.tsx
+++ src/components/game/Clouds.tsx
@@ -1,14 +1,33 @@
 import React from 'react'
+import './Clouds.css'
+
+interface CloudProps {
+  className?: string
+}
+
+function Cloud({ className }: CloudProps) {
+  return (
+    <div className={`cloud ${className ?? ''}`}>
+      <div className="cloud-body">
+        <span className="cloud-puff puff-1" />
+        <span className="cloud-puff puff-2" />
+        <span className="cloud-puff puff-3" />
+        <span className="cloud-base" />
+      </div>
+    </div>
+  )
+}
 
 export default function Clouds() {
   return (
-    <div className="fixed top-0 left-0 w-full h-1/3 pointer-events-none z-10">
-      <div className="absolute top-10 left-20 w-32 h-16 bg-white rounded-full opacity-80 animate-pulse" />
-      <div className="absolute top-20 right-40 w-40 h-20 bg-white rounded-full opacity-70 animate-pulse" style={{ animationDelay: '1s' }} />
-      <div className="absolute top-5 left-1/2 w-36 h-18 bg-white rounded-full opacity-75 animate-pulse" style={{ animationDelay: '2s' }} />
-      <div className="absolute top-30 left-1/3 w-28 h-14 bg-white rounded-full opacity-85 animate-pulse" style={{ animationDelay: '0.5s' }} />
-      {/* Sol */}
-      <div className="absolute top-10 right-10 w-20 h-20 bg-yellow-300 rounded-full shadow-lg shadow-yellow-400/50 animate-pulse" />
+    <div className="clouds">
+      <div className="cloud-sun" />
+      <Cloud className="cloud-1" />
+      <Cloud className="cloud-2" />
+      <Cloud className="cloud-3" />
+      <Cloud className="cloud-4" />
+      <Cloud className="cloud-5" />
+      <Cloud className="cloud-6" />
     </div>
   )
 }
